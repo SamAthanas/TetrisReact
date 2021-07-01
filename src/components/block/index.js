a7bd4e00f21ca9317a628bd1385af9bf80f0e874
Sam Athanas <sjathana@asu.edu>
2021-07-01 14:15:22 -0700

    Control Updates

--- src/components/block/index.js
+++ src/components/block/index.js
@@ -1,7 +1,19 @@
 import styles from "./block.module.scss";
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from "../../constants";
+import { useState } from "react";
 
-export default function block() {
+export default function Block(props) {
+    const [position,setPosition] = useState(props.position); // [0,100]
+
     return (
-        <div className = {styles.block}></div>
+        <div 
+            className = {styles.block}
+            style = {
+                {
+                    left:`${position}%`
+                }
+            }
+        >  
+        </div>
     );
 }
--- src/pages/tetris/index.js
+++ src/pages/tetris/index.js
@@ -1,43 +1,48 @@
 import styles from "./tetris.module.scss";
 import Block from "../../components/block";
 
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from "../../constants";
 import { UseKeyPress } from "../../hooks/KeyPress.js";
 import { useEffect, useRef, useState } from "react";
 
 export default function Tetris() {
     const [activeBlocks,setActiveBlocks] = useState(null);
+    const [container,setContainer] = useState();
 
+    const [activeBlockPosition,setActiveBlockPosition] = useState(0);
     const [ keysDown ] = UseKeyPress();
     const frameRef = useRef(null);
-
+    
     useEffect( () => {
         const animate = () => {
-            console.log("animating");
+            const movingLeft = keysDown.includes(37);
+            const movingRight = keysDown.includes(39);
+            
+            if (movingRight)
+                console.log("moving right");
 
+            // setActiveBlockPosition(50);
+            //console.log("animating");
+            
+            setActiveBlocks(
+                
+            );
+            
             frameRef.current = requestAnimationFrame(animate);
         }
-
+        
         animate();
-
-        setActiveBlocks(
-            <Block/>
-        );
-
+            
         return () => {
             cancelAnimationFrame(frameRef.current);
         };
-    },[]);
-
-    useEffect( () => {
-        const movingLeft = keysDown.includes(37);
-        const movingRight = keysDown.includes(39);
-
     },[keysDown]);
 
+    const width = CANVAS_WIDTH,height = CANVAS_HEIGHT;
     return (
         <div className = {styles.wrapper}>
-            <div className = {styles.container}>
-                {activeBlocks}
+            <div className = {styles.container} style = {{width,height}}>
+            <Block position = {activeBlockPosition}/>
             </div>
         </div>
     );
