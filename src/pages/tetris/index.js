5bf7c80ab724b0a8198bcc68b0d299a555aa0c8a
Sam Athanas <sjathana@asu.edu>
2021-07-04 16:20:55 -0700

    Bug fixes

--- src/pages/tetris/index.js
+++ src/pages/tetris/index.js
@@ -321,6 +321,7 @@ export default function Tetris() {
     let scale = 1;
     if (typeof window === 'undefined') {
         global.window = {}
+        return <></>;
     } 
     else {
         scale = TetrisUtility.constrain(window.innerWidth,350,700).map(350,700,0.5,1);
@@ -329,7 +330,7 @@ export default function Tetris() {
     return (<>
         <div className = {styles.wrapper}>
             <div className = {`${styles.text} ${gameOverRef.current && styles.gameover}`}>
-            <svg class="MuiSvgIcon-root jss174" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 16v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-5 0h-2v-2h2v2zm0-4h-2V8h2v4zm-1 10c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2z"></path></svg>
+            <svg className = {styles.bell} focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 16v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-5 0h-2v-2h2v2zm0-4h-2V8h2v4zm-1 10c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2z"></path></svg>
                 <h1>Tetris</h1>
                 <p>Created with Next.JS</p>
                 <h2>Samuel Athanasenas</h2>
